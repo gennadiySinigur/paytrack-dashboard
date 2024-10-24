a1abb77585fbf797d0f2adb4e87527398d701fcb
gennadiySinigur <gennadiysinigur@gmail.com>
2024-10-24 09:00:02 +0300

    🛒 Used store data in component

--- app.vue
+++ app.vue
@@ -4,3 +4,13 @@
     <NuxtWelcome />
   </div>
 </template>
+
+<script setup>
+import { useDashboardStore } from '~/stores/dashboard';
+
+const { financeOverviewData, fetchFinanceOverviewData } = useDashboardStore();
+
+await useAsyncData("dasboardStore", () => fetchFinanceOverviewData());
+
+console.log("financeOverviewData", financeOverviewData);
+</script>
