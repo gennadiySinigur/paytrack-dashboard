630fd2af4bb129f200ad1fc649a139de4dba5d9f
gennadiySinigur <gennadiysinigur@gmail.com>
2024-10-31 07:11:32 +0200

    🌐 Improved Language Button

--- components/LanguageButton.vue
+++ components/LanguageButton.vue
@@ -2,7 +2,7 @@
   <button class="flex items-center">
     <img class="object-cover mr-2" src="/public/flag_en.svg" h-20 w-27 alt="UK flag" />
 
-    <p class="mr-1 text-sm font-500">English</p>
+    <p class="hidden lg:block mr-1 text-sm font-500">English</p>
 
     <i class="i-ic-baseline-arrow-drop-down h-16 w-16"></i>
   </button>
